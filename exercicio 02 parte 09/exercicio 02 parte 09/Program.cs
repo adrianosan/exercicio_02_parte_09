66a8161821d0710e66a355a1e3a21a8c92ff1b4d
adriano.santos <adriano.santos@squadra.com.br>
2019-05-31 10:52:34 -0300

    EXERCICIO 02 PARTE 09

---
+++ exercicio 02 parte 09/exercicio 02 parte 09/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_02_parte_09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "Adriano";
+            Console.Write("Digite um nome ");
+            nome = string.Format(Console.ReadLine());
+
+            if (nome == "Adrino")
+
+            {
+                Console.WriteLine("nome correto.");
+            }
+
+            else
+
+            {
+                Console.WriteLine("nome incorreto.");
+            }
+
+            
+        }
+
+    }
+
+}
+
+
+            
+
+
+
+
+
+
+           
+
+
+
+           
+
+
+
+
+                    
